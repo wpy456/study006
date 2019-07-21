acbd2227342ca9bd6148f741d7065366a2c240ff
wpy456 <wpy456@sohu.com>
2019-07-21 19:42:02 +0800

    Initial Commit

---
+++ study006/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  study006
+//
+//  Created by PeiYu Wang on 2019/7/21.
+//  Copyright © 2019 PeiYu Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
