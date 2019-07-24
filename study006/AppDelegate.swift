a754f61b39c27e2fe8228eee8e4819b786e97d82
wpy456 <wpy456@sohu.com>
2019-07-24 22:31:15 +0800

    0

--- study006/AppDelegate.swift
+++ study006/AppDelegate.swift
@@ -13,32 +13,37 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+//应用程序载入
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print("应用程序载入")
         return true
     }
-
+//将要进入非活动状态
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        print("将要进入非活动状态")
     }
-
+//被推送到后台
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+//将要从后台重回前台
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        print("将要从后台重回前台")
     }
-
+//进入活动状态
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        print("进入活动状态")
     }
-
+//将要退出
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        print("将要退出")
     }
 
 
--- study006/ViewController.swift
+++ study006/ViewController.swift
@@ -11,10 +11,72 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.backgroundColor=UIColor.gray
+
+        let firstView=UIView(frame: CGRect(x: 40, y: 80, width: 100, height: 100))
+        firstView.backgroundColor=UIColor.green
+        firstView.alpha=0.3
+        firstView.bounds=CGRect(x: -10, y: -10, width: 100, height: 100)
+        //当alpha=0 时 当前的UIView及其子视图都会被隐藏，并且从响应链中移除
+        firstView.clipsToBounds=true
+        //子视图超过父视图被切除
+        self.view.addSubview(firstView)
+        
+        let secondView=UIView(frame: CGRect(x: 40, y: 180, width: 100, height: 100))
+        secondView.backgroundColor=UIColor.blue
+//        secondView.isHidden=true
+        //当前的UIView及其子视图都会被隐藏，并且从响应链中移除
+        self.view.addSubview(secondView)
+        
+        let thirdView=UIView(frame: CGRect(x: 0, y: 0, width: 120, height: 120))
+        thirdView.backgroundColor=UIColor.purple
+        let image=UIImage(named: "shouey_banner")
+        thirdView.backgroundColor=UIColor.init(patternImage: image!)
+        firstView.addSubview(thirdView)
+        
+       
+        
+        let testView1=UIView(frame: CGRect(x: 40, y: 300, width: 100, height: 100))
+        testView1.backgroundColor=UIColor.green
+        
+        let testView2=UIView(frame: CGRect(x: 60, y: 320, width: 100, height: 100))
+        testView2.backgroundColor=UIColor.red
+        
+        let testView3=UIView(frame: CGRect(x: 80, y: 340, width: 100, height: 100))
+        testView3.backgroundColor=UIColor.blue
+        
+        self.view.addSubview(testView1)
+        self.view.addSubview(testView2)
+        self.view.addSubview(testView3)
+        
+        self.view.insertSubview(testView3, belowSubview: testView2)//将3移动到2下方
+        self.view.insertSubview(testView3, aboveSubview: testView2)//将3移动到2上方
+        self.view.bringSubviewToFront(testView1)//将1移动到顶部
+        self.view.sendSubviewToBack(testView1)//将1移动到底部
+        testView3.removeFromSuperview()//删除3
+        
+        let testView4=UIView(frame: CGRect(x: 40, y: 460, width: 100, height: 100))
+        testView4.backgroundColor=UIColor.orange
+        self.view.addSubview(testView4)
+        
+        let guesture=UITapGestureRecognizer(target: self, action: #selector(SingleTap))//手势对象
+        testView4.addGestureRecognizer(guesture)//添加手势
+        
+        let testView5=UIView(frame: CGRect(x: 40, y: 580, width: 100, height: 100))
+        testView5.backgroundColor=UIColor.black
+        testView5.center=self.view.center
+        self.view.addSubview(testView5)
+        
+        let transform=testView5.transform//映射
+        testView5.transform=transform.translatedBy(x: 0, y: 100)//变换
+        testView5.transform=transform.scaledBy(x: 1.5, y: 1.5)//变换
     }
-
-
+    //手势响应
+    @objc func SingleTap(){
+        print("<#T##items: Any...##Any#>")
+    }
 }
 
